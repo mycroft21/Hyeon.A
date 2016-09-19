71d18a2738cd00991443e4cc38923893db37e33f
leesolge <leesolge@gmail.com>
2016-09-20 08:44:14 +0900

    메인 컨트롤러 완성!

--- src/kosta125/team3/control/Controller.java
+++ src/kosta125/team3/control/Controller.java
@@ -1,23 +1,32 @@
 package kosta125.team3.control;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Properties;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kosta125.team3.subcon.SubCon;
+
 public class Controller extends HttpServlet{
 
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	/*서브 컨트롤러를 담아놓고 사용할 맵*/
+	private Map classMap = new HashMap<>();
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
 	}
 
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
 	}
 	
 	/*init() method : 서버를 처음 시작할 때 필요한 설정을 합니다.*/
@@ -51,11 +60,54 @@ public class Controller extends HttpServlet{
 		
 		while(proIter.hasNext()){
 			String command = (String) proIter.next();
+			/*1회차에 담기는 값 : command = list.park*/
 			String className = pr.getProperty(command);
-		}
+			/*1회차에 담기는 값 : className = kosta125.team3.subcon.ListAction*/
+			try {
+				Class notClass = Class.forName(className);
+				/*클래스로서의 기능을 하도록 해당 경로명의 클래스를 '선언'*/
+				Object classInstance = notClass.newInstance();
+				/*객체로서의 기능을 하도록 해당 객체를 생성*/
+				classMap.put(command, classInstance);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}/*while*/
 		
 	}
 	
-	
+	protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println(classMap);
+		String view = null;
+		SubCon subcon = null;
+		
+		try {
+			String fullCommand = request.getRequestURI();
+			/*fullCommand = hyeonA/List.park*/
+			System.out.println("fullCommand : "+fullCommand);//확인용
+			String projectName = request.getContextPath();
+			/*projectName = hyeonA*/
+			String classKey = null;
+			/*이 변수에는 fullCommand - projectName이 들어갈 예정*/
+			
+			if(fullCommand.indexOf(projectName)==0){
+				classKey = fullCommand.substring(projectName.length()+1);
+				System.out.println(classKey);
+			}
+			
+			subcon = (SubCon) classMap.get(classKey);
+			System.out.println("subcon : "+subcon);
+			view = subcon.process(request, response);
+			System.out.println("view : "+view);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		request.setAttribute("CONTENT", view);
+		RequestDispatcher rd = request.getRequestDispatcher("template/template.jsp");
+		rd.forward(request, response);
+		/*해당 내용을 템플릿으로 보냅니다.*/
+	}
 	
 }

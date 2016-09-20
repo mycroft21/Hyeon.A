d3a490a4fbfd017a48c5adfd0a81753077b0a57c
leesolge <leesolge@gmail.com>
2016-09-20 21:16:45 +0900

    리스트 구현 완료

--- src/kosta125/team3/subcon/ListAction.java
+++ src/kosta125/team3/subcon/ListAction.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kosta125.team3.park.ParkDAO;
+import kosta125.team3.park.ParkVO;
 
 public class ListAction implements SubCon{
 
@@ -16,14 +17,58 @@ public class ListAction implements SubCon{
 		List list2 = dao.list(fnum, 2);
 		List list3 = dao.list(fnum, 3);
 		List list4 = dao.list(fnum, 4);
+		List list5 = dao.list(fnum, 5);
 		System.out.println(list1);
 		System.out.println(list2);
 		System.out.println(list3);
 		System.out.println(list4);
-		request.setAttribute("list1", list1);
-		request.setAttribute("list2", list2);
-		request.setAttribute("list3", list3);
-		request.setAttribute("list4", list4);
+		System.out.println(list5);
+		
+		for(int i=1;i<=list1.size();i++){
+			ParkVO temp = (ParkVO) list1.get(i-1);
+			String parkNum = temp.getParkNum();
+			parkNum = parkNum.substring(3);
+			temp.setParkNum(parkNum);
+			String ts = "avo"+String.valueOf(i);
+			request.setAttribute(ts, temp);			
+		}
+		
+		for(int i=1;i<=list2.size();i++){
+			ParkVO temp = (ParkVO) list2.get(i-1);
+			String parkNum = temp.getParkNum();
+			parkNum = parkNum.substring(3);
+			temp.setParkNum(parkNum);
+			String ts = "bvo"+String.valueOf(i);
+			request.setAttribute(ts, temp);			
+		}
+		
+		for(int i=1;i<=list3.size();i++){
+			ParkVO temp = (ParkVO) list3.get(i-1);
+			String parkNum = temp.getParkNum();
+			parkNum = parkNum.substring(3);
+			temp.setParkNum(parkNum);
+			String ts = "cvo"+String.valueOf(i);
+			request.setAttribute(ts, temp);			
+		}
+		
+		for(int i=1;i<=list4.size();i++){
+			ParkVO temp = (ParkVO) list4.get(i-1);
+			String parkNum = temp.getParkNum();
+			parkNum = parkNum.substring(3);
+			temp.setParkNum(parkNum);
+			String ts = "dvo"+String.valueOf(i);
+			request.setAttribute(ts, temp);			
+		}
+
+		for(int i=1;i<=list5.size();i++){
+			ParkVO temp = (ParkVO) list5.get(i-1);
+			String parkNum = temp.getParkNum();
+			parkNum = parkNum.substring(3);
+			temp.setParkNum(parkNum);
+			String ts = "evo"+String.valueOf(i);
+			request.setAttribute(ts, temp);			
+		}
+		
 		return "/park/list.jsp";
 	}
 	

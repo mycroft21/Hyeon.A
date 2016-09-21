8cb4b5e2879f91a1c8dca1741e9b33892e225c62
jinga117 <w3224@daum.net>
2016-09-21 16:13:28 +0900

    주차 등록 regist 비완성

--- src/kosta125/team3/subcon/RegistFormAction.java
+++ src/kosta125/team3/subcon/RegistFormAction.java
@@ -1,12 +1,60 @@
 package kosta125.team3.subcon;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import kosta125.team3.park.ParkDAO;
+import kosta125.team3.park.ParkVO;
 
 public class RegistFormAction implements SubCon {
 
 	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		return null;
+		String fnum = request.getParameter("fnum");
+		ParkDAO dao = ParkDAO.getInstance();
+		List list1 = dao.list(fnum, 1);
+		List list2 = dao.list(fnum, 2);
+		List list3 = dao.list(fnum, 3);
+		List list4 = dao.list(fnum, 4);
+		List list5 = dao.list(fnum, 5);
+		System.out.println(list1);
+		System.out.println(list2);
+		System.out.println(list3);
+		System.out.println(list4);
+		System.out.println(list5);
+		
+		for(int i=1;i<=list1.size();i++){
+			ParkVO temp = (ParkVO) list1.get(i-1);
+			String ts = "avo"+String.valueOf(i);
+			request.setAttribute(ts, temp);			
+		}
+		
+		for(int i=1;i<=list2.size();i++){
+			ParkVO temp = (ParkVO) list2.get(i-1);
+			String ts = "bvo"+String.valueOf(i);
+			request.setAttribute(ts, temp);			
+		}
+		
+		for(int i=1;i<=list3.size();i++){
+			ParkVO temp = (ParkVO) list3.get(i-1);
+			String ts = "cvo"+String.valueOf(i);
+			request.setAttribute(ts, temp);			
+		}
+		
+		for(int i=1;i<=list4.size();i++){
+			ParkVO temp = (ParkVO) list4.get(i-1);
+			String ts = "dvo"+String.valueOf(i);
+			request.setAttribute(ts, temp);			
+		}
+
+		for(int i=1;i<=list5.size();i++){
+			ParkVO temp = (ParkVO) list5.get(i-1);
+			String ts = "evo"+String.valueOf(i);
+			request.setAttribute(ts, temp);			
+		}
+		
+		return "/park/registForm.jsp";
 	}
 
 }
--- src/kosta125/team3/subcon/RegistProAction.java
+++ src/kosta125/team3/subcon/RegistProAction.java
@@ -3,12 +3,26 @@ package kosta125.team3.subcon;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kosta125.team3.park.ParkDAO;
+import kosta125.team3.park.ParkVO;
+
 public class RegistProAction implements SubCon {
 
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		request.setCharacterEncoding("UTF-8");
+		ParkVO vo = new ParkVO();
+		
+		vo.setCarNum(request.getParameter("carNum"));
+		vo.setParkNum(request.getParameter("parkNum"));
+		
+		ParkDAO dao = ParkDAO.getInstance();
+		dao.regist(vo);
+		
+		request.setAttribute("carNum", vo.getCarNum());
+		request.setAttribute("parkNum", vo.getParkNum());
+		
+		return "/park/registPro.jsp";
 	}
 
 }

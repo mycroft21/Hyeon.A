8800a72d824848ad0250285b81dc2ac332f5724a
leesolge <leesolge@gmail.com>
2016-09-27 13:36:28 +0900

    메모 DB varchar>>varchar2로 수정

--- WebContent/DB/memoDB.sql
+++ WebContent/DB/memoDB.sql
@@ -2,12 +2,14 @@ create table memoDB(memoNum number primary key,
 					subject varchar2(50), 
 			 		content varchar2(2000),
 					memoTime date,
-					pass varchar(20));
+					pass varchar2(20));
 
 create SEQUENCE mNumbers  
        START WITH 1   
        INCREMENT BY 1;
 
+alter table memoDB modify (pass varchar2(20))
+       
 insert into memoDB (memoNum, subject, content, memotime) values (mNumbers.nextval, '첫메모', '테스트합니다.', sysdate);
 
 select * from memoDB;

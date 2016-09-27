6cf211c6becbca1ea268adf544bc650a63f67c70
leesolge <leesolge@gmail.com>
2016-09-27 17:09:37 +0900

    디비수정

--- WebContent/DB/memoDB.sql
+++ WebContent/DB/memoDB.sql
@@ -8,7 +8,7 @@ create SEQUENCE mNumbers
        START WITH 1   
        INCREMENT BY 1;
 
-alter table memoDB modify (pass varchar2(20));
+alter table memoDB modify (subject varchar2(100));
        
 insert into memoDB (memoNum, subject, content, memotime, pass) values (mNumbers.nextval, '메모', '테스트합니다.', sysdate, 'oracle');
 

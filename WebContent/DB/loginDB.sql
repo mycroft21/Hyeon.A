728b049ea3eccda7a1fbb927b7bf52c9f045585a
leesolge <leesolge@gmail.com>
2016-09-28 12:18:33 +0900

    아이디 수정

--- WebContent/DB/loginDB.sql
+++ WebContent/DB/loginDB.sql
@@ -1,7 +1,7 @@
 create table adminslog (id varchar2(20) primary key,
 						pwd varchar2(20) not null)
-insert into adminslog (id, pwd) values ('administrator', 'oracle')
-
+insert into adminslog (id, pwd) values ('admin', 'oracle')
+update ADMINSLOG set id='admin' where pwd='oracle'
 select * from ADMINSLOG
 select * from adminslog where id='administrator' and pwd='oracle'
 drop table adminslog
